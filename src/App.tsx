750857357c074d697ec213d4c2d81b4d9f9d660f
dodonmountain <lkh151515@gmail.com>
2020-11-13 23:48:28 +0900

    첫 커밋!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './views/components/Header'
+
+// 리액트란?
+// UI 라이브러리
+
+// 프론트엔드가 하는일? 
+// 백엔드에서 데이터를 가져와서, 사용자가 보기 편한 상태로 보여준다. -끝.
+
+// 2014년 이전, <- 프론트엔드의 개념 자체가 희박.
+// 프론트엔드가 일을 더 많이한다. 집에는 잘간다. 왜냐면 서비스는 백엔드서만 돌아가니까.
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/views/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// const => 변수 선언 constant 약자 ! 주의 상수는 아님
+// let => 변수 선언 재선언 불가 재할당 가능
+
+const Header = () => {
+  return <h1 className="bigone">하이</h1>;
+}
+
+export default Header;
